cf5f6a797ca9863b7678219037d480b82895dd2c
JavierAbollado <124090494+JavierAbollado@users.noreply.github.com>
2023-04-02 13:28:30 +0200

    Update main.py
    Cambios en la distribución de los sprites. Los he agrupado mejor con grupos, para poder hacer las colisiones de una forma más efectiva. Además he añadido una visualización al juego más amigable. Por último, este ahora necesita de un script auxiliar, constantes.py, en el que cargaremos los variables globales y las imagenes.

--- main.py
+++ main.py
@@ -1,53 +1,14 @@
 import pygame
-import sys, os
 import socket
 import pickle
 import random
 
-# teclas para mover las barras: {s,x} y {k,m}.
-
-# colores
-BLACK = (0, 0, 0)
-GREY = (50,50,50)
-WHITE = (255, 255, 255)
-RED = (255, 0, 0)
-BLUE = (0, 0, 255)
-YELLOW = (255,255,0)
-GREEN = (0,255,0)
-
-BLUE_1 = (0, 50, 255)
-BLUE_2 = (0, 0, 75)
-RED_1 = (255, 0, 0)
-RED_2 = (120, 0, 0)
-
-COLORS_BLOCK = [[RED_1, RED_2], [BLUE_1, BLUE_2]]
-PLAYER_COLOR = [RED, BLUE]
-
-# ejes
-X = 0
-Y = 1
-SIZE = (700, 525)
-
-# player 
-LEFT_PLAYER = 0
-RIGHT_PLAYER = 1
-PLAYER_HEIGHT = 60
-PLAYER_WIDTH = 10
-
-# ball & block
-BALL_COLOR = WHITE
-BALL_SIZE = 12
-BLOCK_SIZE = 40
-FPS = 60
-DELTA = 5 #30
-VEL_BALL_X, VEL_BALL_Y = 2, 3 # velocidad de la bola
-
-SIDES = ["left", "right"]
+from constantes import *
 
 class Player():
     def __init__(self, side):
         self.side = side
-        if side == LEFT_PLAYER:
+        if side == PLAYER_1:
             self.pos = [10, SIZE[Y]//4]
         else:
             self.pos = [10, 3*SIZE[Y]//4]
@@ -76,9 +37,13 @@ class Ball():
         self.color = color
         self.pos=[ SIZE[X]//2, SIZE[Y]//2 ]
         self.velocity = velocity
+        self.alive = True
 
     def get_pos(self):
         return self.pos
+    
+    def kill(self):
+        self.alive = False
 
     def update(self):
         self.pos[X] += self.velocity[X]
@@ -113,6 +78,9 @@ class Game():
     def get_score(self):
         return self.score
 
+    def game_over(self):
+        self.running = False
+
     def is_running(self):
         return self.running
 
@@ -132,29 +100,23 @@ class Game():
             if pos[Y]<0 or pos[Y]>SIZE[Y]:
                 ball.bounce(Y)
             if pos[X]>SIZE[X]:
-                self.score[LEFT_PLAYER] += 1
                 ball.bounce(X)
             elif pos[X]<0:
-                self.score[RIGHT_PLAYER] += 1
-                ball.bounce(X)
+                ball.kill()
 
 
     def __str__(self):
-        return f"G<{self.players[RIGHT_PLAYER]}:{self.players[LEFT_PLAYER]}:{self.ball}>"
+        return f"G<{self.players[PLAYER_2]}:{self.players[PLAYER_1]}:{self.ball}>"
 
 
 class Paddle(pygame.sprite.Sprite):
     def __init__(self, player):
-      super().__init__()
-      self.image = pygame.Surface([PLAYER_WIDTH, PLAYER_HEIGHT])
-      self.image.fill(BLACK)
-      self.image.set_colorkey(BLACK)#drawing the paddle
-      self.player = player
-      color = PLAYER_COLOR[self.player.get_side()]
-      self.color = 0 if self.player.get_side() == 0 else 1
-      pygame.draw.rect(self.image, color, [0,0,PLAYER_WIDTH, PLAYER_HEIGHT])
-      self.rect = self.image.get_rect()
-      self.update()
+        super().__init__()
+        self.player = player
+        self.color = self.player.side
+        self.image = IM_player[self.color]
+        self.rect = self.image.get_rect()
+        self.update()
 
     def update(self):
         pos = self.player.get_pos()
@@ -171,7 +133,6 @@ class BallSprite(pygame.sprite.Sprite):
         self.image = pygame.Surface((BALL_SIZE, BALL_SIZE))
         self.image.fill(BLACK)
         self.image.set_colorkey(BLACK)
-        # pygame.draw.rect(self.image, BALL_COLOR, [0, 0, BALL_SIZE, BALL_SIZE])
         self.rect = self.image.get_rect()
         self.update()
 
@@ -179,6 +140,8 @@ class BallSprite(pygame.sprite.Sprite):
         self.ball.color = 1 - self.ball.color
 
     def update(self):
+        if not self.ball.alive:
+            self.kill()
         color = RED if self.ball.color == 0 else BLUE
         pygame.draw.circle(self.image, color, (BALL_SIZE//2, BALL_SIZE//2), BALL_SIZE//2)
         pos = self.ball.get_pos()
@@ -190,126 +153,122 @@ class Block(pygame.sprite.Sprite):
         super().__init__()
         self.pos = pos
         self.color = color if color != None else random.randint(0,1) # 2 colores € {0,1}
-        self.level = level if level != None else random.randint(0,1) # X niveles € {0,1,...}
-        self.image = pygame.Surface((BLOCK_SIZE, BLOCK_SIZE))
-        self.image.fill(GREY)
-        #self.image.set_colorkey(WHITE)
+        self.level = level if level != None else random.randint(0,3) # X niveles € {0,1,...}
+        self.image = pygame.Surface(BLOCK_SIZE)
+        self.image.fill(BLACK)
+        self.image.blit(IM_block[self.color][self.level], (0.1*BLOCK_SIZE[0], 0.1*BLOCK_SIZE[1]))
         self.rect = self.image.get_rect()
         self.rect.left, self.rect.top = pos
         self.update()
 
+    def get_shot(self):
+        if self.level == 0:
+            self.kill()
+        else:
+            self.level -= 1
+        self.image.blit(IM_block[self.color][self.level], (0.1*BLOCK_SIZE[0], 0.1*BLOCK_SIZE[1]))
+
     def update(self):
-        pygame.draw.rect(self.image, COLORS_BLOCK[self.color][self.level], [BLOCK_SIZE*0.1, BLOCK_SIZE*0.1, BLOCK_SIZE*0.8, BLOCK_SIZE*0.8])
+        pass
+
+class GameOver(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.image = IM_gameover
+        self.rect = self.image.get_rect()
+        self.rect.center = (SIZE[0]//2, SIZE[1]//2)
+    def draw(self, screen):
+        screen.blit(self.image, self.rect)
 
 
 
 class Display():
     def __init__(self, game):
-        self.game = game
-        self.paddles = [Paddle(self.game.get_player(i)) for i in range(2)]
 
-        self.blocks = []
+        self.quit = False
+        self.game = game
+        self.gameover = GameOver()
+
+        # sprite groups
+        self.paddles = pygame.sprite.Group()
+        self.balls = pygame.sprite.Group()
+        self.blocks = pygame.sprite.Group()
+
+        # añadir sprites
+        for padlle in [Paddle(self.game.get_player(i)) for i in range(2)]:
+            self.paddles.add(padlle)
+        for ball in [BallSprite(self.game.get_ball(color=i)) for i in range(2)]:
+            self.balls.add(ball)
         j = 0
-        while 0.75*SIZE[0] + j*(BLOCK_SIZE+5) + BLOCK_SIZE < 0.9*SIZE[0]:
+        while 0.75*SIZE[0] + (j+1)*BLOCK_SIZE[0] < 0.9*SIZE[0]:
             i = 0
-            while 0.1*SIZE[1] + i*(BLOCK_SIZE+5) + BLOCK_SIZE < 0.9*SIZE[1]:
-                block = Block((0.75*SIZE[0] + j*(BLOCK_SIZE+5), 0.1*SIZE[1] + i*(BLOCK_SIZE+5)))
-                self.blocks.append(block)
+            while 0.1*SIZE[1] + (i+1)*BLOCK_SIZE[1] < 0.9*SIZE[1]:
+                block = Block((0.75*SIZE[0] + j*BLOCK_SIZE[0], 0.1*SIZE[1] + i*BLOCK_SIZE[1]))
+                self.blocks.add(block)
                 i += 1
             j += 1
-        self.ball_1 = BallSprite(self.game.get_ball(color=0))
-        self.ball_2 = BallSprite(self.game.get_ball(color=1))
-        self.all_sprites = pygame.sprite.Group()
-        self.paddle_group = pygame.sprite.Group()
-        for block in self.blocks:
-            self.all_sprites.add(block)
-        for paddle  in self.paddles:
-            self.all_sprites.add(paddle)
-            self.paddle_group.add(paddle)
-        self.all_sprites.add(self.ball_1)
-        self.all_sprites.add(self.ball_2)
 
+        # unificar sprites
+        self.all_sprites = pygame.sprite.Group()
+        self.all_sprites.add(self.paddles)
+        self.all_sprites.add(self.balls)
+        self.all_sprites.add(self.blocks)
+
+        # pantalla principal
         self.screen = pygame.display.set_mode(SIZE)
         self.clock =  pygame.time.Clock()  #FPS
-        self.background = pygame.image.load('images/background.png')
-        running = True
         pygame.init()
 
     def analyze_events(self):
-        keys = pygame.key.get_pressed()
-        if keys[pygame.K_s]:
-            self.game.moveUp(LEFT_PLAYER)
-        if keys[pygame.K_x]:
-            self.game.moveDown(LEFT_PLAYER)
-        if keys[pygame.K_k]:
-            self.game.moveUp(RIGHT_PLAYER)
-        if keys[pygame.K_m]:
-            self.game.moveDown(RIGHT_PLAYER)
-        #################
+
+        # comprobar salir del juego
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                self.game.stop()
+                self.quit = True
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_q: # quit
                     self.game.stop()
-                # if event.key == pygame.K_s:
-                #     self.game.moveUp(LEFT_PLAYER)
-                # if event.key == pygame.K_x:
-                #     self.game.moveDown(LEFT_PLAYER)
-                # if event.key == pygame.K_k:
-                #     self.game.moveUp(RIGHT_PLAYER)
-                # if event.key == pygame.K_m:
-                #     self.game.moveDown(RIGHT_PLAYER)
+        
+        # comprobar si ya hemos terminado
+        if not self.game.is_running():
+            return
+        
+        # mover palas
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_s]:
+            self.game.moveUp(PLAYER_1)
+        if keys[pygame.K_x]:
+            self.game.moveDown(PLAYER_1)
+        if keys[pygame.K_k]:
+            self.game.moveUp(PLAYER_2)
+        if keys[pygame.K_m]:
+            self.game.moveDown(PLAYER_2)
+        
         # colisiones BOLA - PALA
-        if pygame.sprite.collide_rect(self.ball_1, self.paddles[0]):
-            if self.ball_1.ball.color != self.paddles[0].color:
-                self.ball_1.change_color()
-            self.ball_1.ball.collide_player()
-        if pygame.sprite.collide_rect(self.ball_1, self.paddles[1]):
-            if self.ball_1.ball.color != self.paddles[1].color:
-                self.ball_1.change_color()
-            self.ball_1.ball.collide_player()
-        if pygame.sprite.collide_rect(self.ball_2, self.paddles[0]):
-            if self.ball_2.ball.color != self.paddles[0].color:
-                self.ball_2.change_color()
-            self.ball_2.ball.collide_player()
-        if pygame.sprite.collide_rect(self.ball_2, self.paddles[1]):
-            if self.ball_2.ball.color != self.paddles[1].color:
-                self.ball_2.change_color()
-            self.ball_2.ball.collide_player()
-        # if pygame.sprite.spritecollide(self.ball_1, self.paddle_group, False):
-        #     self.game.get_ball(color=0).collide_player()
-        # if pygame.sprite.spritecollide(self.ball_2, self.paddle_group, False):
-        #     self.game.get_ball(color=1).collide_player()
-        # colisiones BOLA - BLOQUE
-        for i in range(len(self.blocks)):
-            _break = False
-            for color, ball in enumerate([self.ball_1, self.ball_2]):
-                if pygame.sprite.collide_rect(self.blocks[i], ball):
-                    AXIS = Y if ((abs(self.blocks[i].rect.top - ball.rect.bottom) < self.blocks[i].rect.width*0.1)
-                                or (abs(self.blocks[i].rect.bottom - ball.rect.top) < self.blocks[i].rect.width*0.1)) else X
-                    if ball.ball.color == self.blocks[i].color:
-                        if self.blocks[i].level == 0:
-                            self.blocks[i].kill()
-                            del self.blocks[i]
-                        else:
-                            self.blocks[i].level -= 1
-                    self.game.get_ball(color=color).collide_player(AXIS)
-                    _break = True
-                    break
-            if _break: break
+        for ball, paddles in pygame.sprite.groupcollide(self.balls, self.paddles, False, False).items():
+            paddle = paddles[0]
+            if ball.ball.color != paddle.color:
+                ball.change_color()
+            ball.ball.collide_player()
+        
+        # colisiones BOLA - PALA
+        for ball, blocks in pygame.sprite.groupcollide(self.balls, self.blocks, False, False).items():
+            block = blocks[0]
+            if ball.ball.color == block.color:
+                block.get_shot()
+            AXIS = Y if ((abs(block.rect.top - ball.rect.bottom) < block.rect.width*0.1)
+                            or (abs(block.rect.bottom - ball.rect.top) < block.rect.width*0.1)) else X
+            ball.ball.collide_player(AXIS)
+        if len(self.balls) == 0:
+            self.game.game_over()
         self.all_sprites.update()
 
     def refresh(self):
-        #self.screen.blit(self.background, (0, 0))
-        self.screen.fill(WHITE)
-        # score = self.game.get_score()
-        # font = pygame.font.Font(None, 74)
-        # text = font.render(f"{score[LEFT_PLAYER]}", 1, WHITE)
-        # self.screen.blit(text, (250, 10))
-        # text = font.render(f"{score[RIGHT_PLAYER]}", 1, WHITE)
-        # self.screen.blit(text, (SIZE[X]-250, 10))
-        self.all_sprites.draw(self.screen)
+        if not self.game.is_running():
+            self.gameover.draw(self.screen)
+        else:
+            self.screen.blit(IM_background, (0, 0))
+            self.all_sprites.draw(self.screen)
         pygame.display.flip()
 
     def tick(self):
@@ -342,13 +301,12 @@ class Network:
             print(e)
 
 
-
 def main():
     try:
         game = Game()
         display = Display(game)
 
-        while game.is_running():
+        while not display.quit:
             game.movements()
             display.analyze_events()
             display.refresh()
