8834778503fdc42da08f8c3dc5b591848f1abde2
JavierAbollado <124090494+JavierAbollado@users.noreply.github.com>
2023-04-09 23:57:58 +0200

    Update sprites.py

--- sprites.py
+++ sprites.py
@@ -139,3 +139,78 @@ class LevelComplete(pygame.sprite.Sprite):
         self.rect.center = (SIZE[0]//2, SIZE[1]//2)
     def draw(self, screen):
         screen.blit(self.image, self.rect)
+
+
+class Game:
+
+    def __init__(self):
+
+        # players
+        self.players = [Player(i) for i in range(2)]
+
+        # sprite groups
+        self.paddles = pygame.sprite.Group()
+        self.balls = pygame.sprite.Group()
+        self.blocks = pygame.sprite.Group()
+        self.blocks_new_balls = pygame.sprite.Group()
+
+        # añadir sprites
+        for i in range(2):
+            paddle = Paddle(self.players[i])
+            self.paddles.add(paddle)
+        for _ in range(2):
+            ball = Ball([BALL_VEL[0] * (-1)**random.randint(0,1), -BALL_VEL[1]], color=random.randint(0,1))
+            self.balls.add(ball)
+        j = 0
+        while 0.1*SIZE[0] + (j+1)*BLOCK_SIZE[0] < 0.9*SIZE[0]:
+            i = 0
+            while 0.1*SIZE[1] + (i+1)*BLOCK_SIZE[1] < 0.25*SIZE[1]:
+                block = Block((0.1*SIZE[0] + j*BLOCK_SIZE[0], 0.1*SIZE[1] + i*BLOCK_SIZE[1]))
+                self.blocks.add(block)
+                i += 1
+            j += 1
+        
+        # unificar sprites
+        self.all_sprites = pygame.sprite.Group()
+        self.all_sprites.add(self.paddles)
+        self.all_sprites.add(self.balls)
+        self.all_sprites.add(self.blocks)
+
+        # variables para info
+        self.running = True
+        self.win = False
+
+    def add_new_balls(self, n=2):
+        for _ in range(n):
+            ball = Ball([BALL_VEL[0] * (-1)**random.randint(0,1), -BALL_VEL[1]], color=random.randint(0,1))
+            self.balls.add(ball)
+            self.all_sprites.add(ball)
+
+    def get_player(self, side):
+        return self.players[side]
+
+    def game_over(self, win):
+        self.win = win
+        self.running = False
+
+    def is_running(self):
+        return self.running
+
+    def stop(self):
+        self.running = False
+
+    def moveRight(self, player):
+        self.players[player].moveRight()
+
+    def moveLeft(self, player):
+        self.players[player].moveLeft()
+
+    def movements(self):
+        for ball in self.balls: #[self.ball_1, self.ball_2]:
+            pos = ball.pos
+            if pos[Y]<0:
+                ball.bounce(Y)
+            if pos[X]<0 or pos[X]>SIZE[X]:
+                ball.bounce(X)
+            elif pos[Y]>SIZE[Y]:
+                ball.kill()
